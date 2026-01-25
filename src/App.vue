e72b36970449817ea323ffee28c083db0923f43f
yuecx <yuecx2004@163.com>
2026-01-25 18:03:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <BasicLayout/>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import BasicLayout from '@/layout/BasicLayout.vue'
+</script>
+
+
+<style scoped>
+
+</style>
