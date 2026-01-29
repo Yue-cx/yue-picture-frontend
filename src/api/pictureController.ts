7a5c4a8882dadb8ce7c545b6d33a7e23d605b746
yuecx <yue-cx@qq.com>
2026-01-29 19:58:00 +0800

    feat:uploadPictureByUrl

--- src/api/pictureController.ts
+++ src/api/pictureController.ts
@@ -166,8 +166,22 @@ export async function uploadPictureUsingPost(
       ...params,
     },
     data: formData,
-    // @ts-ignore 临时忽略requestType的类型报错
     requestType: 'form',
+    ...(options || {}),
+  })
+}
+
+/** uploadPictureByUrl POST /api/picture/upload/url */
+export async function uploadPictureByUrlUsingPost(
+  body: API.PictureUploadRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePictureVO_>('/api/picture/upload/url', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
     ...(options || {}),
   })
 }
--- src/pages/picture/AddPicturePage.vue
+++ src/pages/picture/AddPicturePage.vue
@@ -4,7 +4,16 @@
       {{ route.query?.id ? '修改图片' : '创建图片' }}
     </h2>
     <!--图片上传组件-->
-    <PictureUpload :picture="picture" :onSuccess="onSuccess" />
+    <!-- 选择上传方式 -->
+    <a-tabs v-model:activeKey="uploadType"
+    >>
+      <a-tab-pane key="file" tab="文件上传">
+        <PictureUpload :picture="picture" :onSuccess="onSuccess" />
+      </a-tab-pane>
+      <a-tab-pane key="url" tab="URL 上传" force-render>
+        <UrlPictureUpload :picture="picture" :onSuccess="onSuccess" />
+      </a-tab-pane>
+    </a-tabs>
     <!--图片信息表单-->
     <a-form v-if="picture" layout="vertical" :model="pictureForm" @finish="handleSubmit">
       <a-form-item label="名称" name="name">
@@ -54,6 +63,7 @@ import {
   listPictureTagCategoryUsingGet
 } from '@/api/pictureController.ts'
 import { message } from 'ant-design-vue'
+import UrlPictureUpload from '@/components/UrlPictureUpload.vue'
 
 const picture = ref<API.PictureVO>()
 const pictureForm = reactive<API.PictureEditRequest>({})
@@ -62,6 +72,7 @@ const onSuccess = (newPicture: API.PictureVO) => {
   pictureForm.name = newPicture.name
 }
 const router = useRouter()
+const uploadType = ref<'file' | 'url'>('file')
 
 /**
  * 提交表单
